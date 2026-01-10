eff7352b3cf778a2c3186b26a7f325324556e022
Prasoon-dhoundiyal <90601185+Prasoon-dhoundiyal@users.noreply.github.com>
2026-01-10 21:17:34 +1100

    Update plots.py

--- plots.py
+++ plots.py
@@ -158,8 +158,8 @@ def plot_logdiff_4_comparison(
     ax.plot(
         g_con,
         color="#111827",
-        lw=0.6,
-        alpha=0.6,
+        lw=1.0,
+        alpha=1.2,
         label="Constrained (Δ₄ log price)"
     )
 
